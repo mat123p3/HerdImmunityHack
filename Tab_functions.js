c61423873ba5f1d441fa2b03ca73c16cfec9c491
Clean674 <sylvio.poirier.674@gmail.com>
2026-02-07 18:19:33 -0500

    Summary of js changes
    Names for functions changed

--- Tab_functions.js
+++ Tab_functions.js
@@ -1,13 +1,14 @@
 function openTrust(evt, Analysis){
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-        for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
+    var i, Analysis, tablinks;
+    Analysis = document.getElementsByClassName("Analysis");
+        for (i = 0; i < Analysis.length; i++) {
+        Analysis[i].style.display = "none";
     }
     tablinks = document.getElementsByClassName("tablinks");
         for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
-  document.getElementById(cityName).style.display = "block";
+  document.getElementById(InfoType).style.display = "block";
+  document.getElementById("defaultOpen").click();
   evt.currentTarget.className += " active";
 }
